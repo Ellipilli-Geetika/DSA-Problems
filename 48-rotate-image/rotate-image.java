bc6f106497d24e224d5504c4cb35bec0b0751a84
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-04-18 18:22:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (41.23%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -4,18 +4,21 @@ class Solution {
 
         for(int i = 0; i < matrix[0].length ; i++)
         {
-            for(int j = 0 ; j < matrix.length ; j++)
+            for(int j = i+1 ; j < matrix.length ; j++)
             {
-                m[i][j] = matrix[matrix.length-1 - j][i];
-                
+                int temp = matrix[i][j];
+                matrix[i][j]= matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
         
         for(int i = 0; i < matrix[0].length ; i++)
         {
-            for(int j = 0 ; j < matrix.length ; j++)
+            for(int j = 0 ; j < matrix.length / 2 ; j++)
             {
-                matrix[i][j] = m[i][j];
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length-1-j];
+                matrix[i][matrix[0].length-1-j] = temp;
             }
         }
         
