351f55f4ee33bbff6ad383f44a6aba49f3b1a3f2
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-04-21 19:45:51 +0530

    Time: 10 ms (45.69%) | Memory: 48.4 MB (30.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,19 +1,56 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        int count1  = 0 , elem1 = Integer.MIN_VALUE, count2 = 0 , elem2 = Integer.MIN_VALUE;
         List<Integer> l = new ArrayList<Integer>();
+
         for(int i = 0; i < nums.length ; i++)
         {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        int n = nums.length/3;
-        for(int i : map.keySet())
-        {
-            if(map.get(i)>n)
+            if(count1==0 && elem2!=nums[i])
             {
-                l.add(i);
+                count1  = 1;
+                elem1 = nums[i];
+            }
+            else if(count2==0 && elem1!=nums[i])
+            {
+                count2  = 1;
+                elem2 = nums[i];
+            }
+            else if(nums[i]==elem1)
+            {
+                count1++;
+            }
+            else if(nums[i]==elem2)
+            {
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
+        count1=0;
+        count2=0;
+
+        for(int i = 0 ; i< nums.length ; i++)
+            {
+                if(nums[i]==elem1)
+                    count1++;
+                if(nums[i]==elem2)
+                    count2++;
+
+            }
+        System.out.println("elem1 elem2"+elem1+elem2);
+        
+        if(count1>Math.floor(nums.length/3))
+            l.add(elem1);
+        if(count2 > Math.floor(nums.length/3))
+            l.add(elem2);
+    
+
         return l;
+        
+
+
     }
 }
