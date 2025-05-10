cd17631353ba04174ca37c1d78efc61a776205a3
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-05-10 13:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (28.12%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxDepth(String s) {
 
-        Stack<Character> st = new Stack<Character>();
-        int max = 0;
+        
+        int max = 0, count  = 0;
 
         for(int i = 0; i < s.length() ; i++)
         {
             if(s.charAt(i)=='(')
             {
-                st.push(s.charAt(i));
-                max = Math.max(max,st.size());
+                count++;
+                max = Math.max(max,count);
 
             }
             else if(s.charAt(i)==')')
             {
-                st.pop();
+                count--;
             }
         }
         return max;
