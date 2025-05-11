8c02c92e5eb951cdd0c46ae5c85780dd055dbda8
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-05-11 22:44:21 +0530

    Time: 2565 ms (5.04%) | Memory: 45.2 MB (45.59%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int maxLen = 1;
+
+        String maxPal = s.substring(0,1);
+        for(int i = 0; i < s.length() ; i++)
+        {
+            for(int j = i ; j < s.length() ; j++)
+            {
+                if(isPalindrome(s.substring(i,j+1)))
+                {
+                    if(maxLen < (j-i+1))
+                    {
+                        maxLen = j-i+1;
+                        maxPal = s.substring(i,j+1);
+                    }
+                }
+            }
+        }
+        return maxPal;
+      
+
+    }
+
+    public boolean isPalindrome(String str)
+    {
+        int left = 0, right = str.length()-1;
+
+        while(left<=right)
+        {
+            if(str.charAt(left)==str.charAt(right))
+            {
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
