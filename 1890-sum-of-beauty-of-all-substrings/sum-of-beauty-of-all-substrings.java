7f4d96458c185139ca6f0f83807914f3090652a0
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-05-14 22:24:06 +0530

    Time: 94 ms (25.91%) | Memory: 45.2 MB (10.85%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,43 +1,40 @@
 class Solution {
     public int beautySum(String s) {
+        int[][] freq = new int[s.length()+1][26];
 
-        if(s==null || s.length()==0)
-            return 0;
-        
-        
-        int count =0;
         for(int i = 0; i < s.length() ; i++)
         {
-            for(int j = i+1; j <= s.length() ; j++)
+            for(int j = 0; j < 26 ; j++)
             {
-               count += beauty(s.substring(i,j));
+                freq[i+1][j] = freq[i][j];
+
+            }
+            freq[i+1][s.charAt(i) - 'a']++;
+        }
+        int totalBeauty = 0;
+        for(int i = 0; i < s.length() ; i++)
+        {
+            
+            for(int j = i ; j < s.length() ; j++)
+            {
+                int[] fr = new int[26];            
+
+                for(int k = 0 ; k < 26 ; k++)
+                {
+                    fr[k] = freq[j+1][k]-freq[i][k];
+                }
+
+                int maxf = 0;
+                int minf = Integer.MAX_VALUE;
+                for (int f : fr) {
+                    if (f > 0) {
+                        maxf = Math.max(maxf, f);
+                        minf = Math.min(minf, f);
+                    }
+                }
+                totalBeauty += (maxf - minf);
             }
         }
-        return count;
-        
-    }
-
-    public int beauty(String str)
-    {
-        if (str.length() <= 1)
-            return 0;
-
-        int[] freq = new int[26];
-        for (char c : str.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        int maxf = 0;
-        int minf = Integer.MAX_VALUE;
-
-        for (int f : freq) {
-            if (f > 0) {
-                maxf = Math.max(maxf, f);
-                minf = Math.min(minf, f);
-            }
-        }
-
-        return maxf - minf;
-
+        return totalBeauty;
     }
 }
