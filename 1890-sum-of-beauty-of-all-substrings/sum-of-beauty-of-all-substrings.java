079e36ddfd680e7de96cfd2d46dea84092f06404
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-05-14 22:38:29 +0530

    Time: 61 ms (49.92%) | Memory: 44.2 MB (24.31%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -2,34 +2,23 @@ class Solution {
     public int beautySum(String s) {
         int[][] freq = new int[s.length()+1][26];
 
-        for(int i = 0; i < s.length() ; i++)
-        {
-            for(int j = 0; j < 26 ; j++)
-            {
-                freq[i+1][j] = freq[i][j];
-
-            }
-            freq[i+1][s.charAt(i) - 'a']++;
-        }
         int totalBeauty = 0;
         for(int i = 0; i < s.length() ; i++)
         {
+            int[] fr = new int[26];
             
             for(int j = i ; j < s.length() ; j++)
             {
-                int[] fr = new int[26];            
+
+                fr[s.charAt(j)-'a']++;   
+                int maxf = Integer.MIN_VALUE;
+                int minf = Integer.MAX_VALUE;  
 
                 for(int k = 0 ; k < 26 ; k++)
                 {
-                    fr[k] = freq[j+1][k]-freq[i][k];
-                }
-
-                int maxf = 0;
-                int minf = Integer.MAX_VALUE;
-                for (int f : fr) {
-                    if (f > 0) {
-                        maxf = Math.max(maxf, f);
-                        minf = Math.min(minf, f);
+                    if (fr[k] > 0) {
+                        maxf = Math.max(maxf, fr[k]);
+                        minf = Math.min(minf, fr[k]);
                     }
                 }
                 totalBeauty += (maxf - minf);
