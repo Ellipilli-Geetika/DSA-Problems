0220d63b5725b6a34933ab3c8068497d91610b4e
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2025-05-21 22:23:08 +0530

    Time: 12 ms (30.49%) | Memory: 57 MB (39.72%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return mergesort(head);
+        
+    }
+
+    public ListNode findMiddle(ListNode head)
+    {
+        if(head == null || head.next == null)
+            return head;
+        ListNode slow = head, fast = head.next;
+        while(fast!=null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode  mergesort(ListNode head)
+    {
+        if(head == null || head.next == null)
+            return head;
+        ListNode mid = findMiddle(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode left = mergesort(head);
+        right = mergesort(right);
+        return sort(left,right);
+    }
+
+    public ListNode sort(ListNode left,ListNode right)
+    {
+        ListNode temp1 = left, temp2 = right;
+        ListNode l = new ListNode(1);
+        ListNode result = l;
+        
+        while(temp1 != null && temp2 != null)
+        {
+            
+            if(temp1.val <= temp2.val)
+            {
+                result.next = temp1;
+                temp1 = temp1.next;
+            }
+            else if(temp1.val > temp2.val)
+            {
+                result.next = temp2;
+                temp2 = temp2.next;
+            }
+            result = result.next;
+        }
+
+        while(temp1!=null)
+        {
+            result.next = temp1;
+            temp1 = temp1.next;
+            result = result.next;
+
+        }
+        while(temp2!=null)
+        {
+            result.next = temp2;
+            temp2 = temp2.next;
+            result = result.next;
+
+        }
+        return l.next;
+
+    }
+}
