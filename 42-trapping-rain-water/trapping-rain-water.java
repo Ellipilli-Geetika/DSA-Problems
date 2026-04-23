a7d12b9ff1489c4e371edbaf21d4753dd7381d06
Ellipilli-Geetika <66192228+Ellipilli-Geetika@users.noreply.github.com>
2026-04-23 15:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (82.76%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,34 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int[] prefixmax = new int[n];
-        int[] suffixmax = new int[n];
-        prefixmax[0] = 0;
-        suffixmax[n-1] = 0;
-        for(int i = 1; i < n ; i++)
-        {
-            prefixmax[i] = Math.max(prefixmax[i-1],height[i-1]);
-        }
-        for(int i = n-2; i >= 0 ; i--)
-        {
-            suffixmax[i] = Math.max(suffixmax[i+1],height[i+1]);
-        }
-        int sum = 0;
+        int sum = 0, maxLeft = 0, maxRight = 0;
+        int l =0, r = height.length -1;
         
-        for(int i = 0; i < n ; i++)
+        while ( l <= r)   // we need to update maxLeft, maxRight and update sum when something less among maxLeft and maxRight is encountered.
         {
-            int m = Math.min(prefixmax[i],suffixmax[i]);
-            
-            if(m > height[i]) {
-                sum += m - height[i];
+            if(maxLeft <= maxRight)
+            {
+                if(maxLeft < height[l])
+                {
+                    maxLeft = height[l];
+                }
+                else
+                {
+                    sum += maxLeft - height[l];
+                    
+                }
+                l++;
+            }
+            else
+            {
+                if(maxRight < height[r])
+                {
+                    maxRight = height[r];
+                }
+                else
+                {
+                    sum += maxRight - height[r];
+                }
+                r--;
             }
 
         }
